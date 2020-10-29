d89a2b0adb9ef266f9c944496b62ed3037945a34
Brian Hoffman <brian.hoffman0003@temple.edu>
2020-10-29 13:49:34 -0400

    BL-1356 use decimal sorting for numbers
    squash

--- lib/lc_solr_sortable.rb
+++ lib/lc_solr_sortable.rb
@@ -7,17 +7,18 @@ module LcSolrSortable
     _process(call_number.gsub(/(\d)\.(\d)/, '\1!\2').split("."))
   end
 
-  def self._process(data, result = "")
+  def self._process(data, result = "", decimal_sort=true)
 
     if data.is_a? Array
-      return data.map { |segment| _process(segment) }.join("")
+      first = _process(data.shift, "", false)
+      return data.map { |segment| _process(segment) }.unshift(first).join("")
     end
 
     # add interstitial 'Z's and normalize length
     if data && data.downcase.match(/(^[[:alpha:]]+)(.*)/)
       remainder = $2
       alphas = $1.gsub(/([[:alpha:]]{1})/, 'Z\1').ljust(10, "a")
-      return _process(remainder, "#{result}#{alphas}")
+      return _process(remainder, "#{result}#{alphas}", decimal_sort)
     end
 
     # pad left to normalize fractions, then right to normalize length
@@ -28,7 +29,8 @@ module LcSolrSortable
       zeros.times { num.concat("0") }
       raise "call no processing error" unless num.match(/^\d+$/)
       num = num.each_char.to_a.map { |c| @@alphabet[c.to_i] }.join("")
-      return _process(remainder, "#{result}#{num.rjust(10, 'a')}")
+      return _process(remainder, "#{result}#{num.ljust(10, 'a')}", decimal_sort) if decimal_sort
+      return _process(remainder, "#{result}#{num.rjust(10, 'a')}", decimal_sort) unless decimal_sort
     end
 
     return result
--- spec/lc_solr_sortable_spec.rb
+++ spec/lc_solr_sortable_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe LcSolrSortable do
     sorted_numbers = numbers.shuffle.sort_by { |num| LcSolrSortable.convert(num) }
     expect(sorted_numbers).to eq numbers
   end
+
+  it "sorts these call numbers correctly" do
+    numbers = ["M357 .K86 op.86 1900z",
+               "M357.M9 L3 1986",
+               "M357.M85 D5 1942",
+               "M357.M417 C7 1963",
+               "M357.M565 S8",
+               "M357.Q46 S663 1961"]
+    sorted_numbers = numbers.sort_by { |num| LcSolrSortable.convert(num) }
+    expect(sorted_numbers).to eq ["M357 .K86 op.86 1900z",
+                                  "M357.M417 C7 1963",
+                                  "M357.M565 S8",
+                                  "M357.M85 D5 1942",
+                                  "M357.M9 L3 1986",
+                                  "M357.Q46 S663 1961"]
+  end
+
+  it "sorts numbers differently before the decimal" do
+    numbers = ["M20.B121",
+               "M20.B21",
+               "M120.B121",
+               "M120.B21"]
+    sorted_numbers = numbers.reverse.sort_by { |num| LcSolrSortable.convert(num) }
+    expect(sorted_numbers).to eq numbers
+  end
 end
